392a6b99ad8e457143689e7c8d67eede91023201
atomazos <at2276@columbia.edu>
2019-11-29 12:27:04 -0600

    pushed completed UFO-level-1 code

--- UFO-level-1 /static/js/app.js
+++ UFO-level-1 /static/js/app.js
@@ -1,4 +1,88 @@
 // from data.js
-var tableData = data;
+var tbody = d3.select("tbody");
+// console.log(data);
 
-// YOUR CODE HERE!
+// Step 1: Loop Through `data` and console.log each ufo report object
+// data.forEach(function(ufoReport) {
+// console.log(ufoReport)
+// });
+
+//Step 2:  Use d3 to append one table row `tr` for each ufo report object
+
+// data.forEach(function(ufoReport) {
+//     console.log(ufoReport);
+//     var row = tbody.append("tr");
+// });
+
+// // Step 3:  Use `Object.entries` to console.log each ufo report value
+
+// data.forEach(function(ufoReport) {
+//     console.log(ufoReport);
+//     var row = tbody.append("tr");
+
+//     Object.entries(ufoReport).forEach(function([key, value]) {
+//         console.log(key, value);
+//           });
+        
+// });
+
+// // Step 4: Use d3 to append 1 cell per ufo report value (date,time,city,state,country,shape,duration,Minutes,comments)
+
+// data.forEach(function(ufoReport) {
+//   console.log(ufoReport);
+//   var row = tbody.append("tr");
+
+//   Object.entries(ufoReport).forEach(function([key, value]) {
+//       console.log(key, value);
+//       var cell = row.append("td");
+//     });
+      
+// });
+
+// Step 5: Use d3 to update each cell's text with ufo report values (date,time,city,state,country,shape,duration,Minutes,comments)
+function populateTable(arr){
+  tbody.html("")
+arr.forEach(function(ufoReport) {
+  // console.log(ufoReport);
+  var row = tbody.append("tr");
+
+  Object.entries(ufoReport).forEach(function([key, value]) {
+      // console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+
+
+  });
+}
+populateTable(data)
+// var enterADate = data
+var enterDate = data
+d3.select("#filter-btn").on("click", getData);
+
+function getData(){
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime-form-input");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+// console.log(inputValue);
+// console.log(enterDate);
+  if (inputValue ===""){
+    populateTable(data)
+  } else{
+    var filteredData = enterDate.filter(enterDate=> enterDate.datetime === inputValue);
+  
+  populateTable(filteredData)
+  }
+  
+
+console.log(filteredData);
+
+
+
+};
+      
+
